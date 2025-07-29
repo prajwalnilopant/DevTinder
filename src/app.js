9c11b3d673e4d49f905172fac1abcdc2a3ae56b8
Prajwal Nilopant <prajwalnilopant@gmail.com>
2025-07-29 12:07:40 -0400

    API-level-sanitizations

--- src/app.js
+++ src/app.js
@@ -64,10 +64,20 @@ app.delete("/user", async (req, res) => {
 });
 
 // Updating the data of the User.
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
   const data = req.body;
   try {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+    const isUpdateAllowed = Object.keys(data).every((k) => ALLOWED_UPDATES.includes(k));
+    // Below two conditionals are API Level Sanitizations/ Validations
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+    if (data?.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
+    }
+
     const user = await User.findByIdAndUpdate(userId, data, {
       returnDocument: "after",
       runValidators: true,
