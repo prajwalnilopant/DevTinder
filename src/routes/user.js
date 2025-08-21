28e5bff24f14692e92f824ab9e89b4fe45577ad6
Prajwal Nilopant <prajwalnilopant@gmail.com>
2025-08-20 21:20:06 -0400

    Building Feed API and Pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const userRouter = express.Router();
 
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const USER_FIELDS = "firstName lastName photoUrl about skills age gender";
 
@@ -42,6 +43,38 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     res.json({ data });
   } catch (err) {
     res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit; // Limit to a maximum of 50 users per page
+    const skip = (page - 1) * limit;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const excludedUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      excludedUsersFromFeed.add(req.fromUserId.toString());
+      excludedUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const allowedUsersOnFeed = await User.find({
+      $and: [{ _id: { $nin: Array.from(excludedUsersFromFeed) } }, { _id: { $ne: loggedInUser._id } }],
+    })
+      .select(USER_FIELDS)
+      .skip(skip)
+      .limit(limit);
+
+    res.json({ data: allowedUsersOnFeed });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
 });
 
