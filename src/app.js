79f2e5be1c3bc58a533079e06a6ff49e94dbcbaf
Prajwal Nilopant <prajwalnilopant@gmail.com>
2025-08-23 09:59:15 -0400

    Made changes to fix CORS error while building Login page

--- src/app.js
+++ src/app.js
@@ -2,10 +2,19 @@ const express = require("express");
 const connectDB = require("./config/database"); // This establishes the link between the database and our App
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 // The below method is used to Parse the JSON data into JS Object. Without parsing we cannot use JSON objects directly within JS.
 app.use(express.json());
 app.use(cookieParser()); // This method is used to read the cookies. Without this, we would get undefined if there is no parser.
+// This is used to allow CORS (Cross-Origin Resource Sharing) so that our frontend can communicate with the backend.
+// Make sure to replace the origin with your frontend URL in production.
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Replace with your frontend URL
+    credentials: true, // Allow cookies to be sent
+  })
+);
 
 const requestRouter = require("./routes/requests");
 const profileRouter = require("./routes/profile");
